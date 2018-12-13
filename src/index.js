d3445e4c771c437aa7a4d4239c1b0d5e57e27146
NielsHarbo <44673313+NielsHarbo@users.noreply.github.com>
2018-12-13 10:34:52 +0100

    First commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const root = document.createElement("div");
+root.innerHTML = '<p>Hello Webpack.</p>';
+const domAppp = document.querySelector('#dom-app');
+domAppp.appendChild(root);
+import './style.scss';
