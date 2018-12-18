929c0a9d659c372b11e58fa496e8bf3190d340ce
NielsHarbo <44673313+NielsHarbo@users.noreply.github.com>
2018-12-18 09:26:57 +0100

    Index updated again

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ const root = document.createElement("div");
 root.innerHTML = '<p>Hello Webpack.</p>';
 const domAppp = document.querySelector('#dom-app');
 domAppp.appendChild(root);
+const openers = document.querySelectorAll('.opener');
+for (let i = 0; i < openers.length; i++) {
+  openers[i].addEventListener('click', function() {
+    this.parentElement.classList.toggle('expanded');
+  });
+}
 //import './style.scss';
